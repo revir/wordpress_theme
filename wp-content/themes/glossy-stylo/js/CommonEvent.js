0e457a5d002239f856e9465a3a0bc9805573fb05
revir <revir.qing@gmail.com>
2013-07-17 20:29:56 +0800

    modified CommonEvent.js

--- wp-content/themes/glossy-stylo/js/CommonEvent.js
+++ wp-content/themes/glossy-stylo/js/CommonEvent.js
@@ -1,16 +1,17 @@
-var header_abstop = $("#header").getBoundingClientRect().top;
-var sidebar_left = $("#sidebar").getBoundingClientRect().left;
+var header_abstop = document.getElementById("header").getBoundingClientRect().top;
+var sidebar_left = document.getElementById("sidebar").getBoundingClientRect().left;
+var sidebar_top = document.getElementById("sidebar").getBoundingClientRect().top;
 
 window.onscroll = function () {
-	var header = $("#header");
-	var sidebar = $("#sidebar");
-	var headertext = $("#header h1 a");
+	var header = document.getElementById("header");
+	var sidebar = document.getElementById("sidebar");
+	//var headertext = document.getElementById("header h1 a");
 	var scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
 	
-	if (sidebar.getBoundingClientRect().top < 0){
+	if (scrolltop > sidebar_top){
 		sidebar.style.position = "fixed";
-		sidebar.style.left = sidebar_left;
-		sidebar.style.top = "0 px";
+		sidebar.style.left = sidebar_left+1+"px";
+		sidebar.style.top = "0";
 	}
 	else{
 		sidebar.style.position = "";
