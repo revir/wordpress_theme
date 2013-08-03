ef7f49c81739fdfdf51eb955f6e019d926e2bc23
revir <revir.qing@gmail.com>
2013-08-03 04:24:22 -0600

    update the theme and add twenty-two to repo

--- wp-content/themes/glossy-stylo/index.php
+++ wp-content/themes/glossy-stylo/index.php
@@ -10,8 +10,7 @@ get_header(); ?>
 				<div class="date"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_time('F jS, Y') ?></a> <!-- by <?php the_author() ?> --></div>
 
 				<div class="entry">
-					<?php the_content('阅读全文...'); ?>
-					
+					<?php the_content('Read the rest of this entry &raquo;'); ?>
 				</div>
 
 				<p class="postmetadata"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> |  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?> <?php edit_post_link('Edit Entry', '| <strong>', '</strong>'); ?> </p>
